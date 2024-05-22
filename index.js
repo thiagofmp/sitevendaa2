496b6ef6197a9eb153719a57252d1b5718d54435
thiagofmp <160027666+thiagofmp@users.noreply.github.com>
2024-05-21 22:04:55 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const botao = document.getElementById("btn")
+
+botao.addEventListener("click", fimcompra)
+
+function fimcompra(){
+    alert("Compra efetuada com sucesso");
+    window.open("index.html")
+    window.CloseEvent
+}
