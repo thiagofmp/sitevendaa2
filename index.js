5b3be12f26413fd84532ef20659aa5c2f0341d74
thiagofmp <160027666+thiagofmp@users.noreply.github.com>
2024-05-21 22:02:34 -0300

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const botao = document.getElementById("btn")
-
-botao.addEventListener("click", fimcompra)
-
-function fimcompra(){
-    alert("Compra efetuada com sucesso");
-    window.open("index.html")
-    window.CloseEvent
-}
